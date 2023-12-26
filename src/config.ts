af33fe9b0e3d65f61a788e2acfa1c1e219267f9f
Peter Kudelas <storm.sk123@gmail.com>
2023-12-26 13:24:00 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const url = "https://now.peterkudelas.eu";
-export const title = "Now | Peter Kudeláš";
+export const title = "Now - Peter Kudeláš | Full Stack Developer";
 export const description =
   "Discover what I am currently up to with my Now Page. Stay updated on my latest web development projects, interests, and activities.";
